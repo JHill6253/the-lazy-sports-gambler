3c23a7558a61e2b5d4cd0a9d8a695da2cec26e4d
John Hill <62448643+JHill6253@users.noreply.github.com>
2021-12-02 18:45:52 -0600

    API reconfig

---
+++ Lazy_API/Text_Service/TextService.py
@@ -0,0 +1,24 @@
+
+
+import os
+# os.environ['TWILIO_ACCOUNT_SID'] = 'SK980a6050ecb8d6dd6b81978ac6699c46'
+# os.environ['TWILIO_AUTH_TOKEN'] = '308676297e51cc80872e60553f0257f7'
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+class textCLI:
+    def __init__(self):
+        self.account_sid = 'ACbe09a36be57c1ee1615bcedeebc1b9ea'
+        self.auth_token = '308676297e51cc80872e60553f0257f7'
+        self.client = Client(self.account_sid, self.auth_token)
+    def sendMessage(self, number, messageBody):
+        message = self.client.messages \
+                    .create(
+                        body=messageBody,
+                        from_='+18339482016',
+                        to='+1'+ number
+                    )
+
+        return message.sid
---
+++ Lazy_API/app.py
@@ -0,0 +1,123 @@
+from flask import Flask, jsonify, request, Response
+import pandas as pd
+import requests
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import time
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import datetime
+import json
+from dateutil import parser
+from datetime import datetime, timezone
+import nba_api
+from nba_api.stats.static import teams, players
+from nba_api.stats.endpoints import (
+    playercareerstats,
+    leaguegamefinder,
+    playerdashboardbyclutch,
+    playergamelogs,
+    commonplayerinfo,
+    teamplayeronoffdetails,
+    teamgamelogs,
+)
+from nba_api.live.nba.endpoints import scoreboard
+
+# from  ML_Models.model import Model
+from Text_Service.TextService import textCLI
+import pymongo
+from pymongo import MongoClient
+import logging as log
+
+
+app = Flask(__name__)
+
+def cleanGames(data):
+    games = []
+    for game in data:
+            games.append(
+                {"gameId":game["gameId"], 
+                "home":{
+                    "teamAbrev" : game['homeTeam']['teamTricode'],
+                    "teamId":game['homeTeam']['teamId']
+                    },
+                "away":{
+                    "teamAbrev" : game['homeTeam']['teamTricode'],
+                    "teamId":game['homeTeam']['teamId']
+                    }
+                    })
+    return {"games": games }
+
+
+
+
+@app.route('/games', methods=['GET',"POST"])
+def games():
+    board = scoreboard.ScoreBoard()
+    print("ScoreBoardDate: " + board.score_board_date)
+    games = board.games.get_dict()
+    temp = cleanGames(games)
+    
+    url = "http://localhost:5001/mongodb"
+    if request.method == 'POST':
+        json_data = {"database": "nba_test", "collection": "game", "Document":temp}
+        headers = {'Content-type':'application/json', 'Accept':'application/json'}
+        response = requests.post(url, json= json_data, headers=headers)
+        return jsonify({'games sent to db': response.json()})
+    return jsonify({"working":"yes"})
+
+@app.route('/createuser', methods=["POST"])
+def createUser():    
+    _json = request.json    
+    url = "http://localhost:5001/mongodb"
+    if request.method == 'POST':
+        json_data = {"database": "nba_test", "collection": "user", "Document":_json}
+        headers = {'Content-type':'application/json', 'Accept':'application/json'}
+        response = requests.post(url, json= json_data, headers=headers)
+        return jsonify({'user created': response.json()})
+    return jsonify({"working":"yes"})
+
+@app.route('/fetchUsers', methods=['GET'])
+def text():
+    url = "http://localhost:5001/mongodb"
+    payload = json.dumps({
+    "database": "nba_test",
+    "collection": "user"
+    })
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+
+    
+    return {'response': response.text}
+
+@app.route('/sendmessages', methods=['POST'])
+def SendMessage():
+    url = "http://localhost:5001/mongodb"
+    payload = json.dumps({
+    "database": "nba_test",
+    "collection": "user"
+    })
+    headers = {
+    'Content-Type': 'application/json'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload).json()
+    print(response)
+    responses = []
+    for res in response:
+        if res["sendText"] == "True":
+            cli = textCLI()
+            messageRes = cli.sendMessage(str(res["phone"]),res["name"]+ ", This is a test of your life alery system")
+            responses.append(messageRes)
+        
+    return jsonify({'response': responses})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5002)
